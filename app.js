7aea3f25cb1a2c3f9f2254fe07a43607ca365faa
malykhs1 <malykhs1955@yandex.ru>
2022-04-01 11:38:41 +0300

    cors fix

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ const { DATABASE } = require('./configs');
 
 const options = {
   origin: [
-    'http://malykhs-diplom.nomoredomains.work',
     'https://malykhs-diplom.nomoredomains.work',
-    'http://api.malykhs-diplom.nomoredomains.rocks',
     'http://localhost:3001',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
